4ec088295675b9544093404ad236e21c4e3d6a08
Alano Morais <alanomorais@gmail.com>
2022-06-09 20:27:50 -0300

    criação da class de conexão e instalação do composer

--- app/db/Connection.php
+++ app/db/Connection.php
@@ -5,12 +5,14 @@ namespace App\Db;
 use PDO;
 use PDOException;
 
-class Connect{
+class Connect
+{
     const HOST = 'localhost';
+    const PORT = 'localhost';
     const NAME = 'entrega';
     const USER = 'root';
     const PASS = '321321321';
-    
+
     private $connection;
 
     public function __construct()
@@ -18,8 +20,15 @@ class Connect{
         $this->connectDatabase();
     }
 
-    private function connectDatabase()
+    function connectDatabase()
     {
-        $this->connection = new 
+        try {
+            //$this->connection = new PDO('pgsql:host=' . self::HOST . ';port=' . self::PORT . ';dbname=' . self::NAME, self::USER, self::PASS);
+            $this->connection = new PDO('mysql:host=' . self::HOST . ';dbname=' . self::NAME, self::USER, self::PASS);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            
+            die('Erro: Conexão com banco de dados não foi realizada com sucesso. Erro gerado <br />' . $e->getMessage());
+        }
     }
 }
---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd5efb821be728b5b31cbf25c32e90c4002c1be63',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd5efb821be728b5b31cbf25c32e90c4002c1be63',
+            'dev_requirement' => false,
+        ),
+    ),
+);
